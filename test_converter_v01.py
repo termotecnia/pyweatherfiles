234559b353d1860209f213ec3422d6f0efae4060
Daniel Sánchez-García <daniel.sanchezgarcia@uca.es>
2026-03-26 19:49:50 +0100

    updated input xlsx

--- test_converter_v01.py
+++ test_converter_v01.py
@@ -2,7 +2,7 @@ import os
 import traceback
 from pyweatherfiles.hourly_epw_converter import HourlyEPWConverter
 
-base_path = "MADRID_Present.epw"
+base_path = "ESP_Madrid.082210_IWEC.epw"
 excel_path = "Madrid_Definitivo_para_convertir_a_epw.xlsx"
 
 # # Configure the mappings to match the provided excel columns
@@ -32,22 +32,36 @@ converter = HourlyEPWConverter(
 )
 
 # Process the first year available to test transform_to_epw
-year = converter.available_years[0]
-df_y = converter.get_year_data(year)
-output_path = f"TEST_output_{year}.epw"
-print(f"Testing transform_to_epw for year {year}...")
-success = converter.transform_to_epw(df_y, base_path, output_path)
-if success:
-    print(f"Success! Output file generated: {output_path}")
-else:
-    print("Failed during transform_to_epw")
+# year = converter.available_years[0]
+# df_y = converter.get_year_data(year)
+# output_path = f"TEST_output_{year}.epw"
+# print(f"Testing transform_to_epw for year {year}...")
+# success = converter.transform_to_epw(df_y, base_path, output_path)
+# if success:
+#     print(f"Success! Output file generated: {output_path}")
+# else:
+#     print("Failed during transform_to_epw")
 
 # Process all available years
-success_list = converter.process(base_path)
+success_list = converter.process(base_epw_path=base_path, output_pattern='{city}_{year}.epw', city='madrid')
 print(f"Success for years: {success_list}")
 
 ##
 
+from pyweatherfiles.hourly_epw_converter import BatchHourlyEPWConverter
+
+cities_config = BatchHourlyEPWConverter.suggest_config(
+    identifiers=['madrid'],
+    data_files=['Madrid_Definitivo_para_convertir_a_epw.xlsx'],
+    base_epw_files=['ESP_Madrid.082210_IWEC.epw']
+)
+
+converter = BatchHourlyEPWConverter(cities_config=cities_config)
+
+converter.process_all()
+
+
+##
 from besos.eplus_funcs import run_building, run_energyplus
 from besos.eppy_funcs import get_building
 
@@ -62,6 +76,15 @@ from besos.eppy_funcs import get_building
 
 run_energyplus(
     building_path='SF_Detached_D_min_South.idf',
-    epw='MADRID_2005.0_convertido.epw',
+    epw='madrid_2025.epw',
     out_dir='eplus_results'
+)
+
+##
+
+from pyweatherfiles.epw_comparator import create_comparison_hourly_dataframe
+
+df = create_comparison_hourly_dataframe(
+    base_epw_path='madrid_2005.epw',
+    generated_epw_path='Madrid_Definitivo_para_convertir_a_epw_2005.epw'
 )
